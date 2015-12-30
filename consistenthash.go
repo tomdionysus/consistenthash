dc9defc979ab924b81bd58fb3c63091d4236163b
Tom Cully <tom.cully@apitalent.co.nz>
2015-12-30 16:43:33 +1300

    Fixed package header godoc errata

--- consistenthash.go
+++ consistenthash.go
@@ -2,5 +2,5 @@
 //
 // The core type is ServerNetworkNode, which respresents a single node in the network - i.e. its unique ID, Host address and distribution. ServerNode represents the local server node, allowing other ServerNetworkNodes to be registered and deregistered, also the 'primary' node for any given key can be found by calling the GetNodeFor method on the ServerNode.
 //
-// For more information on consistent hashing please see https://en.wikipedia.org/wiki/Consistent_hashing)
+// For more information on consistent hashing please see https://en.wikipedia.org/wiki/Consistent_hashing
 package consistenthash
